6b33963b80b986b87327bc0073c6d370744f62fd
walter79 <gzwalter79@gmail.com>
2016-07-15 18:59:46 +0200

    enable adb by default Conflicts: 	device_alifep1pro.mk

--- device_alifep1pro.mk
+++ device_alifep1pro.mk
@@ -203,4 +203,5 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 	ro.debuggable=1 \
 	ro.adb.secure=0 \
 	persist.service.acm.enable=0 \
-	ro.oem_unlock_supported=1
+	ro.oem_unlock_supported=1 \
+	persist.sys.usb.config=mtp,adb
