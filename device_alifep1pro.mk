285312bc142977967ad7e550a1512d97fd421744
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-07-30 00:33:04 +0200

    add experimental goodix fingerprint libs

--- device_alifep1pro.mk
+++ device_alifep1pro.mk
@@ -85,6 +85,11 @@ PRODUCT_PACKAGES += \
 	AntHalService \
 	com.dsi.ant.antradio_library
 
+# Goodix Fingerprint
+PRODUCT_PACKAGES += \
+	GoodixNewFpSetting
+
+
 # ANT Permissions
 PRODUCT_COPY_FILES += \
 	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
